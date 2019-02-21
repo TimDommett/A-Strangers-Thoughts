6136de46a825dbc56107196991e1e572423fc137
Tim Dommett <timdom11@gmail.com>
2019-02-22 00:16:47 +0200

    added user validations

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true
+
 end
