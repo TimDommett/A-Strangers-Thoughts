d216e16a3d03c7673996cb4d6e88c3fd2656b8d2
Tim Dommett <timdom11@gmail.com>
2019-02-22 02:59:08 +0200

    Added user authentication and association to articles

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }, format: { with: VALID_EMAIL_REGEX }
   validates :email, presence: true, uniqueness: true
+  has_many :articles
+  before_save { self.email = email.downcase }
+
 
 end
